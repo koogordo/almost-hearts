4b8c26d0f2ecce1521d0dda071f9be1f80ff614a
Joseph Goergen <jgoergen15@winona.edu>
2018-09-23 16:17:11 -0500

    Added comments to Client class

--- src/AppTools/Client.java
+++ src/AppTools/Client.java
@@ -2,7 +2,14 @@ package AppTools;
 
 import java.io.IOException;
 import java.net.*;
-
+/*
+ * Client constructor sends out a broadcast packet to any DatagramSocket receiving on port 12343
+ * The contents of the sent packet is the name of the user joining (in bytes)
+ * Then waits for a response back from the server with a receiving port 12344
+ * 		(The reason for using a different port for the case that another client doesn't accidently connect to this client)
+ * The response is received and uses the ip-address that it received from the server to 
+ * attempt a TCP connection with server using the port 12345.
+ */
 public class Client {
 	Socket socket;
 	public Client(String name) {
@@ -12,9 +19,7 @@ public class Client {
 			ds = new DatagramSocket(12344);
 			ds.setBroadcast(true);
 			byte[] bytesToSend = name.getBytes();
-			
 			DatagramPacket dp = new DatagramPacket(bytesToSend, bytesToSend.length, InetAddress.getByName("255.255.255.255"), 12343);
-			
 			System.out.println("Attempting to find server");
 			ds.send(dp);
 			ds.receive(dp);
