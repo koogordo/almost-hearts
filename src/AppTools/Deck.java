28b4ce27f52c806767eb7aafbe8d3be7e712215d
CPelzer11 <33434840+CPelzer11@users.noreply.github.com>
2018-09-25 17:42:31 -0500

    Added Card and Deck

---
+++ src/AppTools/Deck.java
@@ -0,0 +1,43 @@
+package AppTools;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Deck{
+
+	List<Card> deck = new ArrayList<>();
+	
+	public Deck() {
+		
+		for(int i=2; i < 15; i++) {
+			Card temp = new Card("H",i);
+			deck.add(temp);
+		}
+		
+		for(int i=2; i < 15; i++) {
+			Card temp = new Card("C",i);
+			deck.add(temp);
+		}
+		
+		for(int i=2; i < 15; i++) {
+			Card temp = new Card("D",i);
+			deck.add(temp);
+		}
+		
+		for(int i=2; i < 15; i++) {
+			Card temp = new Card("S",i);
+			deck.add(temp);
+		}
+		
+		
+	}
+	
+	public void Shuffle() {
+		
+	Collections.shuffle(deck);
+		
+	}
+	
+}
