5aadad71f6d3ca4727635ebf1ed062a0a07bbf74
Joseph Goergen <jgoergen15@winona.edu>
2018-09-25 06:49:01 -0500

    Server comments

--- src/AppTools/Server.java
+++ src/AppTools/Server.java
@@ -8,7 +8,19 @@ public class Server implements Runnable{
 	Socket[] sockets;
 	String[] usernames;
 	DatagramSocket ds;
-	
+	/*
+	 * Class Server implements Runnable so that it is possible to make this a separate thread because the
+	 * machine running the Server is also going to be a Client(player).
+	 * 
+	 * The class starts off by making a Server socket with the port 12345 for later use.
+	 * Next it initializes the array of sockets and String(which is going to store the Clients connection and username).
+	 * Now it creates a DatagramSocket with the port 12343.
+	 * Server waits for incoming packets using the 12343 port and when it receives one, it parses the contents to the String array
+	 * because that is going to contain the Clients username.
+	 * It sends a response DatagramPacket back to the client who requested it(so the client has the ip address of the server).
+	 * Then it waits for an incoming tcp connection request (socket) and saves it in the socket array which holds the connections.
+	 * 
+	 */
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -18,16 +30,19 @@ public class Server implements Runnable{
 			usernames = new String[3];
 			ds = new DatagramSocket(12343);
 			DatagramPacket dp = new DatagramPacket(new byte[0],0);
-			for(int i = 0; i < 3; ++i) {
+			for(int i = 0; i < 1; ++i) {
 				System.out.println("Server - Ready to receive request");
 				ds.receive(dp);	
 				usernames[i] = dp.getData().toString();
+				System.out.println("Server - Received request from " + usernames[i]);
 				dp = new DatagramPacket(new byte[0], 0, InetAddress.getLocalHost(), 12344);
 				ds.send(dp);
 				sockets[i] = server.accept();
 			}
 			
 			//need to make Runnable class to make each socket it's own thread
+			//recommended that the Runnable class contain a reference to this server(or different shared resources)
+			
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
