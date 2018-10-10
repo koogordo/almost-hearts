b369eacfe06e4148c41929f90964bcce8e3bfd68
CPelzer11 <33434840+CPelzer11@users.noreply.github.com>
2018-10-10 15:51:54 -0500

    font sizing

--- src/Views/GameboardGui.java
+++ src/Views/GameboardGui.java
@@ -104,7 +104,7 @@ public class GameboardGui extends JFrame implements Runnable
 		notificationPanel.setLayout(new BoxLayout(notificationPanel, BoxLayout.Y_AXIS));
 		notificationLabel = new JLabel("notification label");
 		notificationLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		notificationLabel.setFont(new Font("Serif", Font.PLAIN, 30));
+		notificationLabel.setFont(new Font("Serif", Font.PLAIN, 40));
 		notificationPanel.add(notificationLabel);
 		contentPane.add(panel);
 		contentPane.add(handArea);
@@ -118,7 +118,7 @@ public class GameboardGui extends JFrame implements Runnable
 		playerLabels[0] = new JLabel("Player 1");
 		playerLabels[0].setVerticalAlignment(SwingConstants.TOP);
 		playerLabels[0].setHorizontalAlignment(SwingConstants.CENTER);
-		playerLabels[0].setFont(new Font("Serif", Font.PLAIN, 20));
+		playerLabels[0].setFont(new Font("Serif", Font.PLAIN, 25));
 		cardLabels[0] = new JLabel();
 		panel_1.setLayout(new BorderLayout(0, 0));
 		panel_1.add(playerLabels[0], BorderLayout.NORTH);
@@ -132,7 +132,7 @@ public class GameboardGui extends JFrame implements Runnable
 		playerLabels[1] = new JLabel("Player 2");
 		playerLabels[1].setHorizontalAlignment(SwingConstants.CENTER);
 		playerLabels[1].setVerticalAlignment(SwingConstants.TOP);
-		playerLabels[1].setFont(new Font("Serif", Font.PLAIN, 20));
+		playerLabels[1].setFont(new Font("Serif", Font.PLAIN, 25));
 		cardLabels[1] = new JLabel();
 		panel_2.setLayout(new BorderLayout(0, 0));
 		panel_2.add(playerLabels[1], BorderLayout.NORTH);
@@ -146,7 +146,7 @@ public class GameboardGui extends JFrame implements Runnable
 		playerLabels[2] = new JLabel("Player 3");
 		playerLabels[2].setVerticalAlignment(SwingConstants.TOP);
 		playerLabels[2].setHorizontalAlignment(SwingConstants.CENTER);
-		playerLabels[2].setFont(new Font("Serif", Font.PLAIN, 20));
+		playerLabels[2].setFont(new Font("Serif", Font.PLAIN, 25));
 		cardLabels[2] = new JLabel();
 		panel_3.setLayout(new BorderLayout(0, 0));
 		panel_3.add(playerLabels[2], BorderLayout.NORTH);
